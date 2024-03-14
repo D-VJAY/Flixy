63952678eca932a23371349cbb925420cc88b415
D-VJAY <157447391+D-VJAY@users.noreply.github.com>
2024-03-14 10:44:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes,Route, HashRouter } from 'react-router-dom';
+import { Routes,Route, BrowserRouter } from 'react-router-dom';
 import Navigation from '../src/components/Navigation';
 import Home from '../src/components/Home';
 import Watchlater from '../src/components/Watchlater';
@@ -9,16 +9,16 @@ import Videoplayer from '../src/components/Videoplayer';
 function App  ()  {
   return (
     <div>
-      <HashRouter basename='/Flixy_OTT'>     
+      <BrowserRouter basename='/Flixy_OTT'>     
       <Navigation/>
       
         <Routes>
-            <Route path="/Flixy_OTT" element={<Home/>}/>
+            <Route path="/Flixy" element={<Home/>}/>
             <Route path="/watchlater" element={<Watchlater/>}/>
             <Route path="/trending" element={<Trending/>}/>
             <Route path="/playit" element={<Videoplayer/>}/>
         </Routes>
-        </HashRouter>
+        </BrowserRouter>
         </div>
   )
 }
